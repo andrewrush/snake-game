4e505396ca45461d110ad04a7da202641d7f5fee
andrewrush <8232067+andrewrush@users.noreply.github.com>
2023-06-26 21:11:08 +0500

    Add new modes and rules
    See snake.js for more info =P

--- snake.js
+++ snake.js
@@ -1,7 +1,25 @@
-//let scoreField = document.getElementById("score-field");
 let foodField = document.getElementById("food-field");
-let score = 0;
+let bestField = document.getElementById("best-field");
+let speed = 2; //default = 1; max = 100 (turbomode)
+
+
 let food = 0;
+//let best = 0;
+let best = 69; //best I've ever seen :D (with body and board rules, 35x35)
+
+foodField.innerText = food;
+bestField.innerText = best;
+
+//rules
+let gameOver = false;
+let bodyRule = true;
+let boardRule = true;
+
+//modes
+let automodeSmart = true;
+let liveFood = false;
+let snakeCrazyness = 0; // from 0 to 100, default = 0; this value using only if automodeSmart === true 
+let foodCrazyness = 56; // from 0 to 100, default = 56; this value using only if liveFood === true 
 
 class SnakeBody {
   constructor(x, y) {
@@ -12,8 +30,8 @@ class SnakeBody {
 
 // board
 let blockSize = 20;
-let cols = 21;
-let rows = 21;
+let cols = 35;
+let rows = 35;
 let blockCount = (cols + rows) / 2;
 let board;
 let context;
@@ -32,15 +50,12 @@ let snakeBody = [];
 // Food
 let foodX;
 let foodY;
+let f_velocityX = 0;
+let f_velocityY = 0;
 
-let gameOver = false;
 
-//smartmode
-let automodeSmart = true;
-// let distToFoodX = foodX - snakeX;
-// let distToFoodY = foodY - snakeY;
-// let distToFoodXabs = Math.abs(distToFoodX);
-// let distToFoodYabs = Math.abs(distToFoodY);
+
+
 
 window.onload = function () {
   board = document.getElementById("board");
@@ -53,92 +68,158 @@ window.onload = function () {
   document.addEventListener("keydown", changeDirection);
 
   //   update();
-  setInterval(update, 1000 / 1000);
+  setInterval(update, 1000 / 10);
+};
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d"); // For drawing on the board
+
+  placeFood();
+
+  document.addEventListener("keydown", changeDirection);
+
+  //   update();
+  setInterval(update, 1000 / (10 * speed));
 };
 
 
 
 function update() {
   if (gameOver) {
-    return;
+	if (food > best) best = food;
+	bestField.innerText = best;
+	
+	snakeX = 10 * blockSize;
+	snakeY = 10 * blockSize;  
+    velocityX = 0;
+	velocityY = 0;
+	
+	food = 0;
+	foodField.innerText = food;
+	
+	snakeBody = [];
+	let f_velocityX = 0;
+	let f_velocityY = 0;
+	placeFood();
+    gameOver = false;
+	//return;
   }
 
+  //отрисовка игрового поля
   context.fillStyle = "#ffd0a4";
   context.fillRect(0, 0, board.width, board.height);
 
-  context.fillStyle = "red";
+  //отрисовка еды
+  //context.fillStyle = "red"; #6699CC
+  context.fillStyle = "#ad3c1f";
   context.fillRect(foodX, foodY, blockSize, blockSize);
 
+   /* обновление координат тела*/
+  //если змея съела еду
   if (snakeX == foodX && snakeY == foodY) {
-    snakeBody.push([foodX, foodY]);
-    placeFood();
-    food ++;
+    snakeBody.push([foodX, foodY]); // увеличиваем хвост
+    placeFood(); // добавляем новую еду
 
+	//увеличиваем счетчик поглощенной пищи
+    food ++;
     foodField.innerText = food;
   }
 
+ 
   for (let i = snakeBody.length - 1; i > 0; i--) {
     snakeBody[i] = snakeBody[i - 1];
   }
 
+  //шея теперь на месте головы
   if (snakeBody.length) {
     snakeBody[0] = [snakeX, snakeY];
   }
-  
+   /* конец обновления координат тела*/
   
 
 
-  context.fillStyle = "green";
-  //обновление головы
+  
+  //обновление координат головы
   snakeX += velocityX * blockSize;
   snakeY += velocityY * blockSize;
-  //отрисовка головы
-  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+  
 
+  //отрисовка тела змеи 
   context.fillStyle = "#4d1700";
-  for (let i = 0; i < snakeBody.length; i++) {
-    let part = snakeBody[i];
-    context.fillRect(
-      part.x * blockCount,
-      part.y * blockCount,
-      blockSize,
-      blockSize
-    );
-  }
+  
+  /// (???) я отключил, ничего не поменялось
+  // for (let i = 0; i < snakeBody.length; i++) {
+    // let part = snakeBody[i];
+    // context.fillRect(
+      // part.x * blockCount,
+      // part.y * blockCount,
+      // blockSize,
+      // blockSize
+    // );
+  // }
 
-  //отрисовка змейки
+  //отрисовка тела, если отключить, тело не будет отображаться
   for (let i = 0; i < snakeBody.length; i++) {
     context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
   }
-
-  // Game over condition
-  if (
-    snakeX < 0 ||
-    snakeX > cols * blockSize ||
-    snakeY < 0 ||
-    snakeY > rows * blockSize
-  ) {
-    gameOver = true;
-    //alert("Game Over!");
-    location.reload();
-  }
-
-  // for (let i = 0; i < snakeBody.length; i++) {
-    // if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
-      // gameOver = true;
-      // alert("Game Over!");
-      // location.reload();
-    // }
-  // }
   
-    //smart direction
+  //отрисовка головы
+  context.fillStyle = "green";
+  context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+  // Условие проигрыша
+  // голова змеи выходит за границы поля
+  if (boardRule) { //правило поля
+	  if (
+		snakeX < 0 ||
+		snakeX > cols * blockSize ||
+		snakeY < 0 ||
+		snakeY > rows * blockSize
+	  ) {
+		gameOver = true;
+		//alert("Game Over!");
+		console.log("Game Over! Food: " + food);
+		//location.reload();
+	  }
+  }
+  if (bodyRule) { //правило тела
+	  for (let i = 0; i < snakeBody.length; i++) {
+		if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+		  gameOver = true;
+		  //alert("Game Over!");
+		  console.log("Game Over! Food: " + food);
+		  //location.reload();
+		}
+	  }
+  }
+  
+    //если змея движется сама
 	if (automodeSmart) {	  
-	smartDirection();  
+		smartDirection();  
 	  	  
   }
+	//если еда движется
+	if (liveFood) {
+		foodDirection(f_velocityX, f_velocityY);
+		//если еда вышла за границы экрана, то она создается заново
+		if (
+			foodX < 0 ||
+			foodX > cols * blockSize ||
+			foodY < 0 ||
+			foodY > rows * blockSize
+		  ) {
+			placeFood();
+		  }	
+	}
+
+	
 
 }
 
+//управление с клавиатуры
 let changeDirection = function (e) {
   if (e.code == "ArrowUp" && velocityY != 1) {
     velocityX = 0;
@@ -166,10 +247,19 @@ let changeDirection = function (e) {
 };
 
 function placeFood() {
-  foodX = Math.floor(Math.random() * cols) * blockSize;
-  foodY = Math.floor(Math.random() * rows) * blockSize;
+	foodX = Math.floor(Math.random() * cols) * blockSize;
+	foodY = Math.floor(Math.random() * rows) * blockSize;
+  
+	//если еда на создалась теле змеи, то создаём заново
+	for (let i = 0; i < snakeBody.length; i++) {
+		if (foodX == snakeBody[i][0] && foodY == snakeBody[i][1]) {
+		  placeFood();
+		}
+	}
+  
 }
 
+//змея оценивает риски 
 function directionSolver() {
 	
 	let t_snakeBody = [];
@@ -193,22 +283,26 @@ function directionSolver() {
 
 	
 	let t_snakeX = snakeX + velocityX * blockSize;
-	let t_snakeY = snakeX + velocityY * blockSize;
+	let t_snakeY = snakeY + velocityY * blockSize;
+	
 	//проверка поражения
-  	if (
+	//правило поля
+	if ( 
 		t_snakeX < 0 ||
-		t_snakeX > cols * blockSize ||
+		(t_snakeX + 1) > cols * blockSize ||
 		t_snakeY < 0 ||
-		t_snakeY > rows * blockSize
+		(t_snakeY + 1) > rows * blockSize
 	  ) {
 		randomDirection();
 	  }
+	
 
-	// for (let i = 0; i < snakeBody.length; i++) {
-		// if (t_snakeX == t_snakeBody[i][0] && t_snakeY == t_snakeBody[i][1]) {
-		  // randomDirection();
-		// }
-	// }
+	for (let i = 0; i < snakeBody.length; i++) {
+		if (t_snakeX == t_snakeBody[i][0] && t_snakeY == t_snakeBody[i][1]) {
+		  randomDirection();
+		}
+	}
+
 }
 
 
@@ -225,7 +319,7 @@ function smartDirection() {
 	let chance =  Math.floor(Math.random() * 100) + 1;
 
 
-	if (chance<=90) {
+	if (chance <= (101 - snakeCrazyness)) {
 		if (distToFoodXabs >= distToFoodYabs) {
 		velocityY = 0;
 
@@ -336,6 +430,139 @@ function randomDirection() {
 	  
 	  
 	directionSolver();
+	  
+	  // if (snake.some((segment, index) => index !== 0 && (Math.abs(segment.x - snake[0].x) <= 1)  && (Math.abs(segment.y - snake[0].y) <= 1))) {
+		
+		// smartDirection();
+		  
+	  // }
+}
+
+function foodDirection(velocityX, velocityY) {
+	
+	let chance =  Math.floor(Math.random() * 100) + 1;
+	if (chance <= (101 - foodCrazyness)) {
+	
+		let direction = Math.floor(Math.random() * 5);
+		
+
+		  if (direction === 0) {
+
+			velocityX = 0;
+
+			velocityY = 0;
+
+		  }
+		  
+		  if (direction === 1) {
+
+			velocityX = -1;
+
+			velocityY = 0;
+
+		  }
+		  if (direction === 2) {
+
+			velocityX = 1;
+
+			velocityY = 0;
+
+		  }
+		  if (direction === 3) {
+
+			velocityX = 0;
+
+			velocityY = -1;
+
+		  }
+		  if (direction === 4) {
+
+			velocityX = 0;
+
+			velocityY = 1;
+
+		  }
+	}
+	else {
+		
+		let distToFoodX = foodX - snakeX;
+		let distToFoodY = foodY - snakeY;
+		let distToFoodXabs = Math.abs(distToFoodX);
+		let distToFoodYabs = Math.abs(distToFoodY);
+		let d_square = distToFoodXabs * distToFoodXabs + distToFoodYabs * distToFoodYabs;  
+		
+		if (d_square >= 100) return;
+		
+		let chance =  Math.floor(Math.random() * 100) + 1;
+
+		
+		if (chance>=90) {
+			if (distToFoodXabs >= distToFoodYabs) {
+			velocityY = 0;
+
+				if (distToFoodX >= 0) {
+					velocityX = 1;
+				}
+				else if (distToFoodX < 0) {
+					velocityX = -1;
+				}
+
+				// else if (distToFoodX === 0) {
+					// randomDirection();
+				// }
+			}
+
+			else if (distToFoodXabs < distToFoodYabs) {
+			 velocityX = 0; 
+				if (distToFoodY >= 0) {
+					velocityY = 1;
+				}
+				else if (distToFoodY < 0) {
+					velocityY = -1;
+				}
+
+				// else if (distToFoodY === 0) {
+					// randomDirection();
+				// }
+			}
+		}
+		
+		else if (distToFoodXabs >= distToFoodYabs) {
+			velocityX = 0;
+
+				if (distToFoodX >= 0) {
+					velocityY = 1;
+				}
+				else if (distToFoodX < 0) {
+					velocityY = -1;
+				}
+
+				// else if (distToFoodX === 0) {
+					// randomDirection();
+				// }
+			}
+
+		else if (distToFoodXabs < distToFoodYabs) {
+		 velocityY = 0; 
+			if (distToFoodY >= 0) {
+				velocityX = 1;
+			}
+			else if (distToFoodY < 0) {
+				velocityX = -1;
+			}
+
+			// else if (distToFoodY === 0) {
+				// randomDirection();
+			// }
+		}
+	
+	}
+	  
+	  foodX += velocityX * blockSize;
+	  foodY += velocityY * blockSize;
+	  
+	  
+	//directionSolver();
 	  
 	  // if (snake.some((segment, index) => index !== 0 && (Math.abs(segment.x - snake[0].x) <= 1)  && (Math.abs(segment.y - snake[0].y) <= 1))) {
 		
